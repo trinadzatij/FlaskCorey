e46c324aa0355d50193343944a3802496619da33
trinadzatij <trinadzatij@gmail.com>
2022-06-26 22:20:01 +0300

    Add registration and login forms with input validation. Fix links with url_for

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 import uuid
 
@@ -23,21 +23,30 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in with {form.email.data}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Please check the username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -7,7 +7,8 @@ class RegistrationForm(FlaskForm):
                             validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password',
+                            validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                             validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
@@ -16,6 +17,7 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password',
+                            validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
